9d58af74c41bbac8e3a7f822110dfee971a4063d
HwangSuhyeon <hwangsu0107@naver.com>
2023-07-19 18:24:48 +0900

    [unrated] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,9 @@
+def solution(cards1, cards2, goal):
+    for g in goal:
+        if len(cards1) != 0 and cards1[0] == g:
+            cards1.pop(0)
+        elif len(cards2) != 0 and cards2[0] == g:
+            cards2.pop(0)
+        else:
+            return 'No'
+    return 'Yes'
