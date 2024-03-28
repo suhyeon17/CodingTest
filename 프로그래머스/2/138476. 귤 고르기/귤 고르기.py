9a9b6fdb8bf1c367b8f209282136ed8c33b4bebc
HwangSuhyeon <hwangsu0107@naver.com>
2024-03-28 17:07:23 +0900

    [level 2] Title: 귤 고르기, Time: 27.26 ms, Memory: 21.3 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,15 +1,13 @@
 from collections import Counter
-
-def solution(k, tangerine):
-    answer = 0
     
-    cnt_dict = Counter(tangerine)
-    cnt = sorted(cnt_dict.items(), key = lambda x:x[1], reverse = True)
-
-    for c in cnt:
-        print(c[1])
-        k -= c[1]
-        answer += 1
-        
+def solution(k, tangerine):
+    cnt = 0
+    tangerine = Counter(tangerine)
+    tangerine = sorted(tangerine.items(), key = lambda x:x[1], reverse = True)
+    
+    for t in tangerine:
+        k -= t[1]
+        cnt += 1
         if k <= 0:
-            return answer
+            return cnt
+        
