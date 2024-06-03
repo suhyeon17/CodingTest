6c02472dcf50ed9c7ff5b8bad1625588a857eb44
HwangSuhyeon <hwangsu0107@naver.com>
2024-06-03 19:46:22 +0900

    [level 2] Title: 짝지어 제거하기, Time: 83.68 ms, Memory: 14.6 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,13 +1,18 @@
+#이중안됨, 뒤에서부터 제거?, 짝짓는거니까 stack?
+#0. stack에 하나 담기
+#1. for alpha in s:
+#2.     stack[-1] == alpha면
+#3.          stack.pop()
 def solution(s):
     stack = []
-    
     for alpha in s:
-        if not stack or alpha != stack[-1]:
+        if not stack or stack[-1] != alpha: #스택이 비거나 같지 않으면
             stack.append(alpha)
-        elif alpha == stack[-1]:
-            stack.pop()
-            
+        else: #비지 않고 같으면
+            stack.pop() 
+    
+    
     if not stack:
         return 1
-    else:
-        return 0
+    else: return 0
+        
