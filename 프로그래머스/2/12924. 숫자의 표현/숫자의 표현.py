948caabbc5b37f1fdcf340f737ab9bc73a01d4ac
HwangSuhyeon <hwangsu0107@naver.com>
2024-06-03 18:52:42 +0900

    [level 2] Title: 숫자의 표현, Time: 4.81 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,14 +1,22 @@
+#연속한 자연수가 중요 조건!!, 2중반복가능
+#1. for i in range(n+1):
+#2.     for j in range(i, n+1):
+#3.         만약 j부터 순차적으로 더해서 15가 될때 cnt += 1 브레이크
+#4.         15가 넘어가면 그냥 안되는거니까 브레이크
+
+
 def solution(n):
     cnt = 0
     
     for i in range(1, n+1):
-        sum = 0
+        total = 0
         for j in range(i, n+1):
-            sum += j
-            if sum == n:
+            total += j
+            if total == n:
                 cnt += 1
                 break
-            elif sum > n:
+            elif total > n:
                 break
+            else: pass
                 
     return cnt
